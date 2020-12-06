f45926b9c33e70d453b326b9ffcc6ea8e772f3bd
Salvo Miosi <salvo.ilmiosi@gmail.com>
2020-12-06 16:49:55 +0100

    fix gestione errori assembler

--- src/compiler/assembler.cpp
+++ src/compiler/assembler.cpp
@@ -26,131 +26,135 @@ void assembler::read_lines(const std::vector<std::string> &lines) {
         }
     };
 
-    for (auto &line : lines) {
-        size_t space_pos = line.find_first_of(' ');
-        std::string cmd = line.substr(0, space_pos);
-        auto arg_str = line.substr(space_pos + 1);
-        auto args = string_split(arg_str, ',');
-        switch (hash(cmd)) {
-        case hash("LABEL"):
-            break;
-        case hash("RDBOX"):
-        {
-            pdf_rect box;
-            box.type = box_type::BOX_RECTANGLE;
-            box.mode = static_cast<read_mode>(std::stoi(args[0]));
-            box.page = std::stoi(args[1]);
-            box.x = std::stof(args[2]);
-            box.y = std::stof(args[3]);
-            box.w = std::stof(args[4]);
-            box.h = std::stof(args[5]);
-            add_command(opcode::RDBOX, std::move(box));
-            break;
-        }
-        case hash("RDPAGE"):
-        {
-            pdf_rect box;
-            box.type = box_type::BOX_PAGE;
-            box.mode = static_cast<read_mode>(std::stoi(args[0]));
-            box.page = std::stoi(args[1]);
-            add_command(opcode::RDPAGE, std::move(box));
-            break;
-        }
-        case hash("RDFILE"):
-        {
-            pdf_rect box;
-            box.type = box_type::BOX_FILE;
-            box.mode = static_cast<read_mode>(std::stoi(args[0]));
-            add_command(opcode::RDFILE, std::move(box));
-            break;
-        }
-        case hash("SETPAGE"):
-        {
-            add_command(opcode::SETPAGE, std::stoi(args[0]));
-            break;
-        }
-        case hash("CALL"):
-        {
-            command_call call;
-            call.name = add_string(args[0]);
-            call.numargs = std::stoi(args[1]);
-            add_command(opcode::CALL, std::move(call));
-            break;
-        }
-        case hash("MVBOX"):         add_command(opcode::MVBOX, std::stoi(arg_str)); break;
-        case hash("THROWERR"):      add_command(opcode::THROWERR); break;
-        case hash("PARSENUM"):      add_command(opcode::PARSENUM); break;
-        case hash("PARSEINT"):      add_command(opcode::PARSEINT); break;
-        case hash("EQ"):            add_command(opcode::EQ); break;
-        case hash("NEQ"):           add_command(opcode::NEQ); break;
-        case hash("AND"):           add_command(opcode::AND); break;
-        case hash("OR"):            add_command(opcode::OR); break;
-        case hash("NOT"):           add_command(opcode::NOT); break;
-        case hash("NEG"):           add_command(opcode::NEG); break;
-        case hash("ADD"):           add_command(opcode::ADD); break;
-        case hash("SUB"):           add_command(opcode::SUB); break;
-        case hash("MUL"):           add_command(opcode::MUL); break;
-        case hash("DIV"):           add_command(opcode::DIV); break;
-        case hash("GT"):            add_command(opcode::GT); break;
-        case hash("LT"):            add_command(opcode::LT); break;
-        case hash("GEQ"):           add_command(opcode::GEQ); break;
-        case hash("LEQ"):           add_command(opcode::LEQ); break;
-        case hash("MAX"):           add_command(opcode::MAX); break;
-        case hash("MIN"):           add_command(opcode::MIN); break;
-        case hash("SELGLOBAL"):     add_command(opcode::SELGLOBAL, add_string(args[0])); break;
-        case hash("SELVAR"):        add_command(opcode::SELVAR, variable_idx(add_string(args[0]), std::stoi(args[1]))); break;
-        case hash("SELVARTOP"):     add_command(opcode::SELVARTOP, add_string(args[0])); break;
-        case hash("SELRANGE"):      add_command(opcode::SELRANGE, variable_idx(add_string(args[0]), std::stoi(args[1]), std::stoi(args[2]))); break;
-        case hash("SELRANGETOP"):   add_command(opcode::SELRANGETOP, add_string(args[0])); break;
-        case hash("SELRANGEALL"):   add_command(opcode::SELRANGEALL, add_string(args[0])); break;
-        case hash("SETDEBUG"):      add_command(opcode::SETDEBUG); break;
-        case hash("CLEAR"):         add_command(opcode::CLEAR); break;
-        case hash("APPEND"):        add_command(opcode::APPEND); break;
-        case hash("SETVAR"):        add_command(opcode::SETVAR); break;
-        case hash("RESETVAR"):      add_command(opcode::RESETVAR); break;
-        case hash("COPYCONTENT"):   add_command(opcode::COPYCONTENT); break;
-        case hash("PUSHNUM"): {
-            if (args[0].find('.') == std::string::npos) {
-                int32_t num = std::stoi(args[0]);
-                if (static_cast<int8_t>(num) == num) {
-                    add_command(opcode::PUSHBYTE, static_cast<int8_t>(num));
-                } else if (static_cast<int16_t>(num) == num) {
-                    add_command(opcode::PUSHSHORT, static_cast<int16_t>(num));
+    try {
+        for (auto &line : lines) {
+            size_t space_pos = line.find_first_of(' ');
+            std::string cmd = line.substr(0, space_pos);
+            auto arg_str = line.substr(space_pos + 1);
+            auto args = string_split(arg_str, ',');
+            switch (hash(cmd)) {
+            case hash("LABEL"):
+                break;
+            case hash("RDBOX"):
+            {
+                pdf_rect box;
+                box.type = box_type::BOX_RECTANGLE;
+                box.mode = static_cast<read_mode>(std::stoi(args[0]));
+                box.page = std::stoi(args[1]);
+                box.x = std::stof(args[2]);
+                box.y = std::stof(args[3]);
+                box.w = std::stof(args[4]);
+                box.h = std::stof(args[5]);
+                add_command(opcode::RDBOX, std::move(box));
+                break;
+            }
+            case hash("RDPAGE"):
+            {
+                pdf_rect box;
+                box.type = box_type::BOX_PAGE;
+                box.mode = static_cast<read_mode>(std::stoi(args[0]));
+                box.page = std::stoi(args[1]);
+                add_command(opcode::RDPAGE, std::move(box));
+                break;
+            }
+            case hash("RDFILE"):
+            {
+                pdf_rect box;
+                box.type = box_type::BOX_FILE;
+                box.mode = static_cast<read_mode>(std::stoi(args[0]));
+                add_command(opcode::RDFILE, std::move(box));
+                break;
+            }
+            case hash("SETPAGE"):
+            {
+                add_command(opcode::SETPAGE, std::stoi(args[0]));
+                break;
+            }
+            case hash("CALL"):
+            {
+                command_call call;
+                call.name = add_string(args[0]);
+                call.numargs = std::stoi(args[1]);
+                add_command(opcode::CALL, std::move(call));
+                break;
+            }
+            case hash("MVBOX"):         add_command(opcode::MVBOX, std::stoi(arg_str)); break;
+            case hash("THROWERR"):      add_command(opcode::THROWERR); break;
+            case hash("PARSENUM"):      add_command(opcode::PARSENUM); break;
+            case hash("PARSEINT"):      add_command(opcode::PARSEINT); break;
+            case hash("EQ"):            add_command(opcode::EQ); break;
+            case hash("NEQ"):           add_command(opcode::NEQ); break;
+            case hash("AND"):           add_command(opcode::AND); break;
+            case hash("OR"):            add_command(opcode::OR); break;
+            case hash("NOT"):           add_command(opcode::NOT); break;
+            case hash("NEG"):           add_command(opcode::NEG); break;
+            case hash("ADD"):           add_command(opcode::ADD); break;
+            case hash("SUB"):           add_command(opcode::SUB); break;
+            case hash("MUL"):           add_command(opcode::MUL); break;
+            case hash("DIV"):           add_command(opcode::DIV); break;
+            case hash("GT"):            add_command(opcode::GT); break;
+            case hash("LT"):            add_command(opcode::LT); break;
+            case hash("GEQ"):           add_command(opcode::GEQ); break;
+            case hash("LEQ"):           add_command(opcode::LEQ); break;
+            case hash("MAX"):           add_command(opcode::MAX); break;
+            case hash("MIN"):           add_command(opcode::MIN); break;
+            case hash("SELGLOBAL"):     add_command(opcode::SELGLOBAL, add_string(args[0])); break;
+            case hash("SELVAR"):        add_command(opcode::SELVAR, variable_idx(add_string(args[0]), std::stoi(args[1]))); break;
+            case hash("SELVARTOP"):     add_command(opcode::SELVARTOP, add_string(args[0])); break;
+            case hash("SELRANGE"):      add_command(opcode::SELRANGE, variable_idx(add_string(args[0]), std::stoi(args[1]), std::stoi(args[2]))); break;
+            case hash("SELRANGETOP"):   add_command(opcode::SELRANGETOP, add_string(args[0])); break;
+            case hash("SELRANGEALL"):   add_command(opcode::SELRANGEALL, add_string(args[0])); break;
+            case hash("SETDEBUG"):      add_command(opcode::SETDEBUG); break;
+            case hash("CLEAR"):         add_command(opcode::CLEAR); break;
+            case hash("APPEND"):        add_command(opcode::APPEND); break;
+            case hash("SETVAR"):        add_command(opcode::SETVAR); break;
+            case hash("RESETVAR"):      add_command(opcode::RESETVAR); break;
+            case hash("COPYCONTENT"):   add_command(opcode::COPYCONTENT); break;
+            case hash("PUSHNUM"): {
+                if (args[0].find('.') == std::string::npos) {
+                    int32_t num = std::stoi(args[0]);
+                    if (static_cast<int8_t>(num) == num) {
+                        add_command(opcode::PUSHBYTE, static_cast<int8_t>(num));
+                    } else if (static_cast<int16_t>(num) == num) {
+                        add_command(opcode::PUSHSHORT, static_cast<int16_t>(num));
+                    } else {
+                        add_command(opcode::PUSHINT, num);
+                    }
                 } else {
-                    add_command(opcode::PUSHINT, num);
+                    add_command(opcode::PUSHFLOAT, std::stof(args[0]));
                 }
-            } else {
-                add_command(opcode::PUSHFLOAT, std::stof(args[0]));
+                break;
             }
-            break;
-        }
-        case hash("PUSHSTR"):
-        {
-            std::string str;
-            if (!(arg_str.front() == '"' ? parse_string : parse_string_regexp)(str, arg_str)) {
-                throw assembly_error(fmt::format("Stringa non valida: {0}", arg_str));
+            case hash("PUSHSTR"):
+            {
+                std::string str;
+                if (!(arg_str.front() == '"' ? parse_string : parse_string_regexp)(str, arg_str)) {
+                    throw assembly_error(fmt::format("Stringa non valida: {0}", arg_str));
+                }
+                add_command(opcode::PUSHSTR, add_string(str));
+                break;
             }
-            add_command(opcode::PUSHSTR, add_string(str));
-            break;
+            case hash("PUSHVAR"):       add_command(opcode::PUSHVAR); break;
+            case hash("JMP"):           add_command(opcode::JMP, getgotoindex(args[0])); break;
+            case hash("JZ"):            add_command(opcode::JZ, getgotoindex(args[0])); break;
+            case hash("JNZ"):           add_command(opcode::JNZ, getgotoindex(args[0])); break;
+            case hash("JTE"):           add_command(opcode::JTE, getgotoindex(args[0])); break;
+            case hash("INC"):           add_command(opcode::INC); break;
+            case hash("DEC"):           add_command(opcode::DEC); break;
+            case hash("ISSET"):         add_command(opcode::ISSET); break;
+            case hash("GETSIZE"):       add_command(opcode::GETSIZE); break;
+            case hash("PUSHCONTENT"):   add_command(opcode::PUSHCONTENT); break;
+            case hash("NEXTLINE"):      add_command(opcode::NEXTLINE); break;
+            case hash("NEXTTOKEN"):     add_command(opcode::NEXTTOKEN); break;
+            case hash("POPCONTENT"):    add_command(opcode::POPCONTENT); break;
+            case hash("NEXTPAGE"):      add_command(opcode::NEXTPAGE); break;
+            case hash("ATE"):           add_command(opcode::ATE); break;
+            case hash("HLT"):           add_command(opcode::HLT); break;
+            default:
+                throw assembly_error(fmt::format("Comando sconosciuto: {0}", cmd));
+            }
         }
-        case hash("PUSHVAR"):       add_command(opcode::PUSHVAR); break;
-        case hash("JMP"):           add_command(opcode::JMP, getgotoindex(args[0])); break;
-        case hash("JZ"):            add_command(opcode::JZ, getgotoindex(args[0])); break;
-        case hash("JNZ"):           add_command(opcode::JNZ, getgotoindex(args[0])); break;
-        case hash("JTE"):           add_command(opcode::JTE, getgotoindex(args[0])); break;
-        case hash("INC"):           add_command(opcode::INC); break;
-        case hash("DEC"):           add_command(opcode::DEC); break;
-        case hash("ISSET"):         add_command(opcode::ISSET); break;
-        case hash("GETSIZE"):       add_command(opcode::GETSIZE); break;
-        case hash("PUSHCONTENT"):   add_command(opcode::PUSHCONTENT); break;
-        case hash("NEXTLINE"):      add_command(opcode::NEXTLINE); break;
-        case hash("NEXTTOKEN"):     add_command(opcode::NEXTTOKEN); break;
-        case hash("POPCONTENT"):    add_command(opcode::POPCONTENT); break;
-        case hash("NEXTPAGE"):      add_command(opcode::NEXTPAGE); break;
-        case hash("ATE"):           add_command(opcode::ATE); break;
-        case hash("HLT"):           add_command(opcode::HLT); break;
-        default:
-            throw assembly_error(fmt::format("Comando sconosciuto: {0}", cmd));
-        }
+    } catch (std::invalid_argument &error) {
+        throw assembly_error(error.what());
     }
 }
