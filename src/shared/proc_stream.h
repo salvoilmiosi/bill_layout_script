d8f26c563e12af1c2c7332e4574e6a822967c787
Salvo Miosi <salvo.ilmiosi@gmail.com>
2020-12-08 12:26:17 +0100

    aggiunto stderr in proc_stream

--- src/shared/proc_stream.h
+++ src/shared/proc_stream.h
@@ -6,12 +6,17 @@
 #include "subprocess.h"
 
 class proc_istreambuf : public std::streambuf {
-public:
-    proc_istreambuf(subprocess &proc) : proc(proc) {}
+private:
+    proc_istreambuf(subprocess &proc, bool is_stderr = false) : proc(proc), is_stderr(is_stderr) {}
 
 protected:
     virtual int underflow() override {
-        int nbytes = proc.read_stdout(BUFSIZE, buffer);
+        int nbytes;
+        if (is_stderr) {
+            nbytes = proc.read_stderr(BUFSIZE, buffer);
+        } else {
+            nbytes = proc.read_stdout(BUFSIZE, buffer);
+        }
         if (nbytes <= 0) {
             return std::char_traits<char>::eof();
         } else {
@@ -22,7 +27,11 @@ protected:
 
 private:
     subprocess &proc;
+    bool is_stderr;
     char buffer[BUFSIZE];
+
+    friend class proc_istream;
+    friend class proc_err_istream;
 };
 
 class proc_istream : public std::istream {
@@ -35,17 +44,37 @@ private:
     proc_istreambuf buffer;
 };
 
-class proc_ostreambuf : public std::streambuf {
+class proc_err_istream : public std::istream {
 public:
+    proc_err_istream(subprocess &proc) : std::istream(nullptr), buffer(proc, true) {
+        std::ios::init(&buffer);
+    }
+
+private:
+    proc_istreambuf buffer;
+};
+
+class proc_ostreambuf : public std::streambuf {
+private:
     proc_ostreambuf(subprocess &proc) : proc(proc) {}
 
 protected:
+    virtual int overflow(int ch) override {
+        if (proc.write_stdin(1, &ch) == 1) {
+            return ch;
+        } else {
+            return std::char_traits<char>::eof();
+        }
+    }
+
     virtual std::streamsize xsputn(const char_type *s, std::streamsize n) override {
         return proc.write_stdin(n, s);
     }
 
 private:
     subprocess &proc;
+
+    friend class proc_ostream;
 };
 
 class proc_ostream : public std::ostream {
