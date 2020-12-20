8dec236c15f64820440566f7e4cbaa00abf27a1e
Salvo Miosi <salvo.ilmiosi@gmail.com>
2020-12-20 18:03:02 +0100

    fix

--- src/reader/functions.cpp
+++ src/reader/functions.cpp
@@ -10,21 +10,21 @@
 
 template<typename T> T convert_var(variable &var) = delete;
 
-template<> const variable    &convert_var<const variable &>    (variable &var) { return var; }
-template<> const std::string &convert_var<const std::string &> (variable &var) { return var.str(); }
-template<> const fixed_point &convert_var<const fixed_point &> (variable &var) { return var.number(); }
+template<> inline const variable    &convert_var<const variable &>    (variable &var) { return var; }
+template<> inline const std::string &convert_var<const std::string &> (variable &var) { return var.str(); }
+template<> inline const fixed_point &convert_var<const fixed_point &> (variable &var) { return var.number(); }
 
-template<> variable     convert_var<variable>   (variable &var) { return std::move(var); }
-template<> std::string  convert_var<std::string>(variable &var) { return std::move(var.str()); }
-template<> fixed_point  convert_var<fixed_point>(variable &var) { return std::move(var.number()); }
+template<> inline variable     convert_var<variable>   (variable &var) { return std::move(var); }
+template<> inline std::string  convert_var<std::string>(variable &var) { return std::move(var.str()); }
+template<> inline fixed_point  convert_var<fixed_point>(variable &var) { return std::move(var.number()); }
 
-template<> int          convert_var<int>        (variable &var) { return var.as_int(); }
-template<> float        convert_var<float>      (variable &var) { return var.number().getAsDouble(); }
-template<> double       convert_var<double>     (variable &var) { return var.number().getAsDouble(); }
-template<> bool         convert_var<bool>       (variable &var) { return var.as_bool(); }
+template<> inline int          convert_var<int>        (variable &var) { return var.as_int(); }
+template<> inline float        convert_var<float>      (variable &var) { return var.number().getAsDouble(); }
+template<> inline double       convert_var<double>     (variable &var) { return var.number().getAsDouble(); }
+template<> inline bool         convert_var<bool>       (variable &var) { return var.as_bool(); }
 
 template<typename T> constexpr bool is_variable = requires(variable v) {
-    convert_var<std::decay_t<T>>(v);
+    convert_var<T>(v);
 };
 
 template<typename T> struct is_optional_impl {
@@ -33,7 +33,7 @@ template<typename T> struct is_optional_impl {
 template<typename T> struct is_optional_impl<std::optional<T>> {
     static constexpr bool value = is_variable<T>;
 };
-template<typename T> constexpr bool is_optional = is_optional_impl<std::decay_t<T>>::value;
+template<typename T> constexpr bool is_optional = is_optional_impl<T>::value;
 
 template<typename T> struct is_vector_impl {
     static constexpr bool value = false;
@@ -41,7 +41,7 @@ template<typename T> struct is_vector_impl {
 template<typename T> struct is_vector_impl<std::vector<T>> {
     static constexpr bool value = is_variable<T>;
 };
-template<typename T> constexpr bool is_vector = is_vector_impl<std::decay_t<T>>::value;
+template<typename T> constexpr bool is_vector = is_vector_impl<T>::value;
 
 template<typename ... Ts> class type_list {
 private:
@@ -71,9 +71,9 @@ template<bool Req> struct check_args_impl<Req> {
 template<bool Req, typename First, typename ... Ts>
 class check_args_impl<Req, First, Ts ...> {
 private:
-    static constexpr bool var = is_variable<First>;
-    static constexpr bool opt = is_optional<First>;
-    static constexpr bool vec = is_vector<First>;
+    static constexpr bool var = is_variable<std::decay_t<First>>;
+    static constexpr bool opt = is_optional<std::decay_t<First>>;
+    static constexpr bool vec = is_vector<std::decay_t<First>>;
 
     using recursive = check_args_impl<Req ? var : !opt, Ts ...>;
 
@@ -90,25 +90,29 @@ template<typename T, typename ... Ts> struct check_args<T(*)(Ts ...)> : public c
 
 using arg_list = my_stack<variable>::range;
 
-template<typename TypeList, size_t I> typename TypeList::get<I> get_arg(arg_list &args) {
-    using type = typename TypeList::get<I>;
+template<typename T> using convert_type =
+    std::conditional_t<is_variable<T>,T,std::decay_t<T>>;
+
+template<typename T, typename InputIt, typename Function>
+constexpr std::vector<T> transformed_vector(InputIt begin, InputIt end, Function fun) {
+    std::vector<T> ret;
+    ret.reserve(end - begin);
+    std::transform(begin, end, std::back_inserter(ret), fun);
+    return ret;
+}
+
+template<typename TypeList, size_t I> inline convert_type<typename TypeList::get<I>> get_arg(arg_list &args) {
+    using type = convert_type<typename TypeList::get<I>>;
     if constexpr (is_optional<type>) {
-        using opt_type = typename std::decay_t<type>::value_type;
+        using opt_type = typename type::value_type;
         if (I >= args.size()) {
             return std::nullopt;
         } else {
             return convert_var<opt_type>(args[I]);
         }
     } else if constexpr (is_vector<type>) {
-        using vec_type = typename std::decay_t<type>::value_type;
-        std::vector<vec_type> varargs;
-        std::transform(
-            args.begin() + I, args.end(), std::back_inserter(varargs),
-            [](variable &var) {
-                return convert_var<vec_type>(var);
-            }
-        );
-        return varargs;
+        using vec_type = typename type::value_type;
+        return transformed_vector<vec_type>(args.begin() + I, args.end(), convert_var<vec_type>);
     } else {
         return convert_var<type>(args[I]);
     }
@@ -154,8 +158,8 @@ static const std::map<std::string, function_handler> lookup {
     create_function("date", [](const std::string &str, const std::string &format, std::optional<std::string> regex, std::optional<int> index) {
         return parse_date(format, str, regex.value_or(""), index.value_or(1));
     }),
-    create_function("month", [](const std::string &str, const std::string &format, std::optional<std::string> regex, std::optional<int> index) {
-        return parse_month(format, str, regex.value_or(""), index.value_or(1));
+    create_function("month", [](const std::string &str, std::optional<std::string> format, std::optional<std::string> regex, std::optional<int> index) {
+        return parse_month(format.value_or(""), str, regex.value_or(""), index.value_or(1));
     }),
     create_function("replace", [](std::string value, const std::string &regex, const std::string &to) {
         return string_replace_regex(value, regex, to);
