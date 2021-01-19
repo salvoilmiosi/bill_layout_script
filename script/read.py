728529326c425f1f71333e4e6ab4d8a303261dbc
Salvo Miosi <salvo.ilmiosi@gmail.com>
2021-01-19 02:59:59 +0100

    migliorato script read

--- script/read.py
+++ script/read.py
@@ -4,19 +4,25 @@ import sys
 import datetime
 import time
 import os
-from multiprocessing.pool import ThreadPool
-from multiprocessing import cpu_count
+from multiprocessing import Pool, cpu_count
 from pathlib import Path
 from termcolor import colored
 
-in_data = []
-results = []
+if len(sys.argv) < 3:
+    print('Argomenti richiesti: input_directory [output] [controllo] [nthreads]')
+    sys.exit()
 
 app_dir = Path(sys.argv[0]).parent
-layout_reader = app_dir.joinpath('../work/bin/reader')
+input_directory = Path(sys.argv[1])
+output_file = Path(sys.argv[2])
+controllo = Path(sys.argv[3] if len(sys.argv) >= 4 else app_dir.joinpath('../work/layouts/controllo.out'))
 
-def check_conguagli():
-    global results
+try:
+    nthreads = int(sys.argv[4]) if len(sys.argv) >= 5 else cpu_count()
+except NotImplementedError:
+    nthreads = 1
+
+def check_conguagli(results):
     sorted_data = []
     error_data = []
     for x in results:
@@ -49,84 +55,72 @@ def check_conguagli():
         old_mesefatt = new_mesefatt
         old_datafatt = new_datafatt
 
-    results = sorted_data + error_data
+    return sorted_data + error_data
 
 def read_pdf(pdf_file):
     rel_path = pdf_file.relative_to(input_directory)
+    ret = {'filename':str(rel_path)}
 
-    # Rilegge i vecchi file solo se il layout e' stato ricompilato
-    ignore = False
-    
-    for old_obj in filter(lambda x: x['filename'] == str(rel_path), in_data):
-        if not 'layout' in old_obj:
-            try:
-                layout = pyreader.getlayout(str(pdf_file), str(controllo))
-                if layout is not None:
-                    old_obj['layout'] = layout
-            except:
-                pass
-        if 'layout' in old_obj and 'values' in old_obj:
-            layout_file = controllo.parent.joinpath('{0}.out'.format(old_obj['layout']))
-            if os.path.getmtime(str(layout_file)) < os.path.getmtime(str(output_file)):
-                results.append(old_obj)
-                ignore = True
-
-    if ignore: return
-
-    file_obj = {'filename':str(rel_path)}
     try:
         out_dict = pyreader.readpdf_autolayout(str(pdf_file), str(controllo))
 
-        file_obj['values'] = out_dict['values']
+        ret['values'] = out_dict['values']
         if 'layout' in out_dict['globals']:
-            file_obj['layout'] = out_dict['globals']['layout'][0]
+            ret['layout'] = out_dict['globals']['layout'][0]
         if 'warnings' in out_dict:
-            file_obj['warnings'] = out_dict['warnings']
+            ret['warnings'] = out_dict['warnings']
             print(colored('{0} ### {1}'.format(rel_path, ', '.join(out_dict['warnings'])), 'yellow'))
-        elif 'layout' in file_obj:
-            print('{0} ### {1}'.format(rel_path, file_obj['layout']))
+        elif 'layout' in ret:
+            print('{0} ### {1}'.format(rel_path, ret['layout']))
         else:
             print(rel_path)
     except pyreader.error as err:
-        file_obj['error'] = str(err)
-        print(colored('{0} ### {1}'.format(rel_path, file_obj['error']), 'red'))
+        ret['error'] = str(err)
+        print(colored('{0} ### {1}'.format(rel_path, ret['error']), 'red'))
     except:
-        file_obj['error'] = 'Errore sconosciuto'
-        print(colored('{0} ### {1}'.format(rel_path, file_obj['error']), 'magenta'))
+        ret['error'] = 'Errore sconosciuto'
+        print(colored('{0} ### {1}'.format(rel_path, ret['error']), 'magenta'))
 
-    results.append(file_obj)
+    return ret
 
-if len(sys.argv) < 3:
-    print('Argomenti richiesti: input_directory [output] [controllo] [nthreads]')
-    sys.exit()
+if __name__ == '__main__':
+    if not input_directory.exists():
+        print(f'La directory {input_directory} non esiste')
+        sys.exit(1)
 
-input_directory = Path(sys.argv[1])
-output_file = Path(sys.argv[2])
-controllo = Path(sys.argv[3] if len(sys.argv) >= 4 else app_dir.joinpath('../work/layouts/controllo.out'))
+    if not controllo.exists():
+        print(f'Il file di layout {controllo} non esiste')
+        sys.exit(1)
+        
+    files = input_directory.rglob('*.pdf')
 
-try:
-    nthreads = int(sys.argv[4]) if len(sys.argv) >= 5 else cpu_count()
-except NotImplementedError:
-    nthreads = 1
+    results = []
 
-if output_file.exists():
-    with open(output_file, 'r') as fin:
-        in_data = json.loads(fin.read())
+    # Rilegge i vecchi file solo se il layout e' stato ricompilato
+    if output_file.exists():
+        with open(output_file, 'r') as fin:
+            in_data = json.loads(fin.read())
 
-if not input_directory.exists():
-    print(f'La directory {input_directory} non esiste')
-    sys.exit(1)
+        in_files = []
+        for f in files:
+            ignore = False
+            rel_path = f.relative_to(input_directory)
+            for old_obj in filter(lambda x: x['filename'] == str(rel_path), in_data):
+                if 'layout' in old_obj and 'values' in old_obj:
+                    layout_file = controllo.parent.joinpath('{0}.out'.format(old_obj['layout']))
+                    if os.path.getmtime(str(layout_file)) < os.path.getmtime(str(output_file)):
+                        results.append(old_obj)
+                        ignore = True
+            if not ignore:
+                in_files.append(f)
+    else:
+        in_files = list(files)
 
-if not controllo.exists():
-    print(f'Il file di layout {controllo} non esiste')
-    sys.exit(1)
+    if in_files:
+        with Pool(min(len(in_files), nthreads)) as pool:
+            results.extend(pool.map(read_pdf, in_files))
 
-files = list(input_directory.rglob('*.pdf'))
+    results = check_conguagli(results)
 
-with ThreadPool(min(len(files), nthreads)) as pool:
-    pool.map(read_pdf, files)
-
-check_conguagli()
-
-with open(output_file, 'w') as fout:
-    fout.write(json.dumps(results))
+    with open(output_file, 'w') as fout:
+        fout.write(json.dumps(results))
