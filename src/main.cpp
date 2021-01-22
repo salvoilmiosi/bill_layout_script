7f3c44e0dcdf0f042549996edae3da8e4c723473
Salvo Miosi <salvo.ilmiosi@gmail.com>
2021-01-22 16:57:22 +0100

    cambiati args reader, aggiunto tool compiler

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,10 @@ public:
     virtual bool OnCmdLineParsed(wxCmdLineParser &parser) override;
 
 private:
-    std::filesystem::path input_file;
-    std::filesystem::path file_pdf;
+    std::filesystem::path input_pdf;
+    std::filesystem::path input_bls;
 
-    bool debug = false;
+    bool show_debug = false;
 
     wxLocale loc = wxLANGUAGE_DEFAULT;
 };
@@ -29,10 +29,9 @@ private:
 wxIMPLEMENT_APP_CONSOLE(MainApp);
 
 static const wxCmdLineEntryDesc g_cmdline_desc[] = {
-    { wxCMD_LINE_OPTION, "p", "pdf", "input pdf", wxCMD_LINE_VAL_STRING, wxCMD_LINE_OPTION_MANDATORY },
-    { wxCMD_LINE_OPTION, "l", "layout-dir", "layout directory", wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL },
-    { wxCMD_LINE_SWITCH, "d", "debug", "debug", wxCMD_LINE_VAL_NONE, wxCMD_LINE_PARAM_OPTIONAL },
-    { wxCMD_LINE_PARAM, nullptr, nullptr, "input layout", wxCMD_LINE_VAL_STRING, wxCMD_LINE_OPTION_MANDATORY },
+    { wxCMD_LINE_SWITCH, "d", "show-debug", "Show Debug Variables", wxCMD_LINE_VAL_NONE, wxCMD_LINE_PARAM_OPTIONAL },
+    { wxCMD_LINE_PARAM, nullptr, nullptr, "input-pdf", wxCMD_LINE_VAL_STRING, wxCMD_LINE_OPTION_MANDATORY },
+    { wxCMD_LINE_PARAM, nullptr, nullptr, "input-bls", wxCMD_LINE_VAL_STRING, wxCMD_LINE_OPTION_MANDATORY },
     { wxCMD_LINE_NONE }
 };
 
@@ -42,51 +41,12 @@ void MainApp::OnInitCmdLine(wxCmdLineParser &parser) {
 }
 
 bool MainApp::OnCmdLineParsed(wxCmdLineParser &parser) {
-    wxString str;
-
-    if (parser.GetParamCount() >= 1) {
-        input_file = parser.GetParam(0).ToStdString();
+    if (parser.GetParamCount() >= 2) {
+        input_pdf = parser.GetParam(0).ToStdString();
+        input_bls = parser.GetParam(1).ToStdString();
     }
-    if (parser.Found("p", &str)) {
-        file_pdf = str.ToStdString();
-    }
-    debug = parser.Found("d");
+    show_debug = parser.Found("d");
     return true;
-}
-
-Json::Value save_output(const reader_output &out, Json::Value &root, bool debug) {
-    auto write_values = [&](const variable_map &values) {
-        Json::Value ret = Json::objectValue;
-        for (auto &[name, var] : values) {
-            if (name.front() == '_' && !debug) {
-                continue;
-            }
-            auto &json_arr = ret[name];
-            if (json_arr.isNull()) json_arr = Json::arrayValue;
-            json_arr.append(var.str());
-        }
-        return ret;
-    };
-
-    root["globals"] = write_values(out.globals);
-    
-    Json::Value &json_values = root["values"] = Json::arrayValue;
-    for (auto &v : out.values) {
-        json_values.append(write_values(v));
-    }
-
-    for (auto &v : out.warnings) {
-        Json::Value &warnings = root["warnings"];
-        if (warnings.isNull()) warnings = Json::arrayValue;
-        warnings.append(v);
-    }
-
-    auto &json_layouts = root["layouts"] = Json::arrayValue;
-    for (auto &l : out.layouts) {
-        json_layouts.append(l);
-    }
-    
-    return root;
 }
 
 int MainApp::OnRun() {
@@ -97,13 +57,46 @@ int MainApp::OnRun() {
         std::cout << result;
         return 1;
     };
+    
+    auto write_values = [&](const variable_map &values) {
+        Json::Value ret = Json::objectValue;
+        for (auto &[name, var] : values) {
+            if (name.front() == '_' && !show_debug) {
+                continue;
+            }
+            auto &json_arr = ret[name];
+            if (json_arr.isNull()) json_arr = Json::arrayValue;
+            json_arr.append(var.str());
+        }
+        return ret;
+    };
 
     try {
-        pdf_document my_doc(file_pdf);
-        reader my_reader(my_doc, bill_layout_script::from_file(input_file));
+        pdf_document my_doc(input_pdf);
+        reader my_reader(my_doc, bill_layout_script::from_file(input_bls));
         my_reader.start();
 
-        std::cout << save_output(my_reader.get_output(), result, debug);
+        const auto &out = my_reader.get_output();
+
+        result["globals"] = write_values(out.globals);
+        
+        Json::Value &json_values = result["values"] = Json::arrayValue;
+        for (auto &v : out.values) {
+            json_values.append(write_values(v));
+        }
+
+        for (auto &v : out.warnings) {
+            Json::Value &warnings = result["warnings"];
+            if (warnings.isNull()) warnings = Json::arrayValue;
+            warnings.append(v);
+        }
+
+        auto &json_layouts = result["layouts"] = Json::arrayValue;
+        for (auto &l : out.layouts) {
+            json_layouts.append(l);
+        }
+
+        std::cout << result;
     } catch (const std::exception &error) {
         return output_error(error.what());
     } catch (...) {
