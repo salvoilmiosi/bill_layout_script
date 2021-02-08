4fd3e2b82953db9606678226b37e494e1ef502b2
Salvo Miosi <salvo.ilmiosi@gmail.com>
2021-02-08 03:52:55 +0100

    costruttore function_handler

--- src/functions.cpp
+++ src/functions.cpp
@@ -114,7 +114,7 @@ template<typename TypeList, size_t I> inline get_nth_t<I, TypeList> get_arg(arg_
 }
 
 template<typename Function>
-inline std::pair<std::string, function_handler> create_function(const std::string &name, Function fun) {
+function_handler::function_handler(Function fun) {
     // l'operatore unario + converte una funzione lambda senza capture
     // in puntatore a funzione. In questo modo il compilatore può
     // dedurre i tipi dei parametri della funzione tramite i template
@@ -125,112 +125,109 @@ inline std::pair<std::string, function_handler> create_function(const std::strin
     // Viene creata una closure che passa automaticamente gli argomenti
     // da arg_list alla funzione fun, convertendoli nei tipi giusti
 
-    function_handler ret([fun](arg_list &&args) {
+    m_fun = [fun](arg_list &&args) {
         return [&] <std::size_t ... Is> (std::index_sequence<Is...>) {
             return fun(get_arg<typename fun_args::types, Is>(args) ...);
         }(std::make_index_sequence<fun_args::types::size>{});
-    });
+    };
 
-    ret.name = name;
-    ret.minargs = fun_args::minargs;
-    ret.maxargs = fun_args::maxargs;
-
-    return {name, std::move(ret)};
+    minargs = fun_args::minargs;
+    maxargs = fun_args::maxargs;
 }
 
 static const std::unordered_map<std::string, function_handler> lookup {
-    create_function("search", [](std::string_view str, std::string_view regex, std::optional<int> index) {
+    {"search", [](std::string_view str, std::string_view regex, std::optional<int> index) {
         return search_regex(regex, str, index.value_or(1));
-    }),
-    create_function("matches", [](std::string_view str, std::string_view regex, std::optional<int> index) {
+    }},
+    {"matches", [](std::string_view str, std::string_view regex, std::optional<int> index) {
         return search_regex_all(regex, str, index.value_or(1));
-    }),
-    create_function("captures", [](std::string_view str, std::string_view regex) {
+    }},
+    {"captures", [](std::string_view str, std::string_view regex) {
         return search_regex_captures(regex, str);
-    }),
-    create_function("date", [](std::string_view str, const std::string &format, std::optional<std::string_view> regex, std::optional<int> index) {
+    }},
+    {"date", [](std::string_view str, const std::string &format, std::optional<std::string_view> regex, std::optional<int> index) {
         return parse_date(format, str, regex.value_or(""), index.value_or(1));
-    }),
-    create_function("month", [](std::string_view str, std::optional<std::string> format, std::optional<std::string_view> regex, std::optional<int> index) {
+    }},
+    {"month", [](std::string_view str, std::optional<std::string> format, std::optional<std::string_view> regex, std::optional<int> index) {
         return parse_month(format.value_or(""), str, regex.value_or(""), index.value_or(1));
-    }),
-    create_function("replace", [](std::string &&value, std::string_view regex, const std::string &to) {
+    }},
+    {"replace", [](std::string &&value, std::string_view regex, const std::string &to) {
         return string_replace_regex(value, regex, to);
-    }),
-    create_function("date_format", [](std::string_view date, const std::string &format) {
+    }},
+    {"date_format", [](std::string_view date, const std::string &format) {
         return date_format(date, format);
-    }),
-    create_function("month_add", [](std::string_view month, int num) {
+    }},
+    {"month_add", [](std::string_view month, int num) {
         return date_month_add(month, num);
-    }),
-    create_function("singleline", [](std::string &&str) {
+    }},
+    {"singleline", [](std::string &&str) {
         return singleline(std::move(str));
-    }),
-    create_function("if", [](bool condition, const variable &var_if, std::optional<variable> var_else) {
+    }},
+    {"if", [](bool condition, const variable &var_if, std::optional<variable> var_else) {
         return condition ? var_if : var_else.value_or(variable::null_var());
-    }),
-    create_function("ifnot", [](bool condition, const variable &var_if, std::optional<variable> var_else) {
+    }},
+    {"ifnot", [](bool condition, const variable &var_if, std::optional<variable> var_else) {
         return condition ? var_else.value_or(variable::null_var()) : var_if;
-    }),
-    create_function("contains", [](std::string_view str, std::string_view str2) {
+    }},
+    {"contains", [](std::string_view str, std::string_view str2) {
         return string_findicase(str, str2, 0) < str.size();
-    }),
-    create_function("substr", [](std::string_view str, int pos, std::optional<int> count) {
+    }},
+    {"substr", [](std::string_view str, int pos, std::optional<int> count) {
         if ((size_t) pos < str.size()) {
             return variable(std::string(str.substr(pos, count.value_or(std::string_view::npos))));
         }
         return variable::null_var();
-    }),
-    create_function("strlen", [](std::string_view str) {
+    }},
+    {"strlen", [](std::string_view str) {
         return str.size();
-    }),
-    create_function("indexof", [](std::string_view str, std::string_view value, std::optional<int> index) {
+    }},
+    {"indexof", [](std::string_view str, std::string_view value, std::optional<int> index) {
         return string_findicase(str, value, index.value_or(0));
-    }),
-    create_function("tolower", [](std::string &&str) {
+    }},
+    {"tolower", [](std::string &&str) {
         return string_tolower(std::move(str));
-    }),
-    create_function("toupper", [](std::string &&str) {
+    }},
+    {"toupper", [](std::string &&str) {
         return string_toupper(std::move(str));
-    }),
-    create_function("isempty", [](const variable &var) {
+    }},
+    {"isempty", [](const variable &var) {
         return var.empty();
-    }),
-    create_function("percent", [](const std::string &str) {
+    }},
+    {"percent", [](const std::string &str) {
         if (!str.empty()) {
             return variable(str + "%");
         } else {
             return variable::null_var();
         }
-    }),
-    create_function("format", [](std::string_view format, std::vector<std::string> args) {
+    }},
+    {"format", [](std::string_view format, std::vector<std::string> args) {
         return string_format(format, args);
-    }),
-    create_function("coalesce", [](std::vector<variable> args) {
+    }},
+    {"coalesce", [](std::vector<variable> args) {
         for (auto &arg : args) {
             if (!arg.empty()) return arg;
         }
         return variable::null_var();
-    }),
-    create_function("strcat", [](std::vector<std::string> args) {
+    }},
+    {"strcat", [](std::vector<std::string> args) {
         std::string var;
         for (auto &arg : args) {
             var += arg;
         }
         return var;
-    }),
-    create_function("max", [](std::vector<variable> args) {
+    }},
+    {"max", [](std::vector<variable> args) {
         if (args.empty()) {
             return variable::null_var();
         }
         return std::move(*std::max_element(args.begin(), args.end()));
-    }),
-    create_function("min", [](std::vector<variable> args) {
+    }},
+    {"min", [](std::vector<variable> args) {
         if (args.empty()) {
             return variable::null_var();
         }
         return std::move(*std::min_element(args.begin(), args.end()));
-    })
+    }}
 };
 
 const function_handler &find_function(const std::string &name) {
--- src/functions.h
+++ src/functions.h
@@ -26,10 +26,19 @@ public:
         : parsing_error(get_message(fun_name, minargs, maxargs), tok) {}
 };
 
-struct function_handler : std::function<variable(arg_list&&)> {
-    std::string name;
+class function_handler {
+public:
     size_t minargs;
     size_t maxargs;
+
+    template<typename Function> function_handler(Function fun);
+
+    variable operator ()(auto &&args) const {
+        return m_fun(std::forward<decltype(args)>(args));
+    }
+
+private:
+    std::function<variable(arg_list&&)> m_fun;
 };
 
 const function_handler &find_function(const std::string &name);
