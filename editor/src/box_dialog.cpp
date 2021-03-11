6ab4a41874a3abc7a449d549d68f634e2135ecef
Salvo Miosi <salvo.ilmiosi@gmail.com>
2021-03-11 22:00:13 +0100

    aggiornati layout

--- editor/src/box_dialog.cpp
+++ editor/src/box_dialog.cpp
@@ -207,8 +207,8 @@ void box_dialog::OnCancel(wxCommandEvent &evt) {
 void box_dialog::OnTest(wxCommandEvent &evt) {
     auto box_copy = m_box;
     TransferDataFromWindow();
+    std::string text = app->getPdfDocument().get_text(m_box);
     m_box = box_copy;
-    std::string text = app->getPdfDocument().get_text(box_copy);
     reader_output->ShowText(wxString(text.c_str(), wxConvUTF8));
 }
 
