a7361e2cf8407bb082ecd8f97af4e189eb113133
Salvo Miosi <salvo.ilmiosi@gmail.com>
2021-04-17 19:48:09 +0200

    aggiunto colonne required

--- readergui/src/gui/dati_fattura.h
+++ readergui/src/gui/dati_fattura.h
@@ -19,6 +19,9 @@ struct default_formatter {
     wxString operator()(const std::string &value) { return wxString::FromUTF8(value.c_str()); }
     wxString operator()(const fixed_point &value) { return fixed_point_to_string(value); }
     wxString operator()(const wxDateTime &value) { return value.Format("%d/%m/%Y"); }
+    wxString operator()(const std::vector<std::string> &value) {
+        return (*this)(string_join(value, "; "));
+    }
 };
 
 template<typename T> inline T variable_to(const variable &var) = delete;
@@ -29,20 +32,24 @@ template<> inline wxDateTime variable_to(const variable &var) { return var.as_da
 struct table_value {
     const char *header;
     int column_width = 80;
+    bool required = false;
 };
 
 struct variable_table_record : std::multimap<std::string, variable> {
     std::string filename;
-    std::string status;
+    std::vector<std::string> status;
 };
 
 template<typename T, typename Formatter = default_formatter>
 struct variable_table_value :  table_value {
+    using value_type = T;
+    using formatter_type = Formatter;
+
     const char *value;
     int index;
 
-    variable_table_value(const char *header, const char *value, int index = 0)
-        : table_value(header)
+    variable_table_value(auto &&args, const char *value, int index = 0)
+        : table_value{std::forward<decltype(args)>(args)}
         , value(value)
         , index(index) {}
     
@@ -60,11 +67,18 @@ struct variable_table_value :  table_value {
 
 template<typename T, typename TVal, typename Formatter = default_formatter>
 struct member_ptr_value : table_value {
+    using value_type = T;
+    using formatter_type = Formatter;
+
     T TVal::* value;
 
-    member_ptr_value(const char *header, T TVal::* value)
-        : table_value(header)
+    member_ptr_value(auto &&args, T TVal::* value)
+        : table_value{std::forward<decltype(args)>(args)}
         , value(value) {}
+
+    std::optional<T> operator()(const TVal &obj) const {
+        return obj.*value;
+    }
 };
 
 template<typename Formatter = default_formatter>
@@ -105,11 +119,11 @@ static inline std::tuple dati_fattura {
     member_ptr_value    {"Status",                  &variable_table_record::status},
     string_table_value  {"Ragione Sociale",         "ragione_sociale"},
     string_table_value  {"Indirizzo Fornitura",     "indirizzo_fornitura"},
-    string_table_value  {"POD",                     "codice_pod"},
-    month_table_value   {"Mese",                    "mese_fattura"},
-    string_table_value  {"Fornitore",               "fornitore"},
-    string_table_value  {"N. Fatt.",                "numero_fattura"},
-    date_table_value    {"Data Emissione",          "data_fattura"},
+    string_table_value  {table_value{.header="POD", .required=true}, "codice_pod"},
+    month_table_value   {table_value{.header="Mese", .required=true}, "mese_fattura"},
+    string_table_value  {table_value{.header="Fornitore", .required=true}, "fornitore"},
+    string_table_value  {table_value{.header="N. Fatt.", .required=true}, "numero_fattura"},
+    date_table_value    {table_value{.header="Data Emissione", .required=true}, "data_fattura"},
     date_table_value    {"Data Scadenza",           "data_scadenza"},
     euro_table_value    {"Costo Materia Energia",   "spesa_materia_energia"},
     euro_table_value    {"Trasporto",               "trasporto_gestione"},
@@ -152,37 +166,47 @@ class VariableMapTable : public DataTableView<variable_table_record> {
 public:
     VariableMapTable(wxWindow *parent, wxWindowID id = wxID_ANY, const wxPoint &position = wxDefaultPosition, const wxSize &size = wxDefaultSize,
         const wxValidator &validator = wxDefaultValidator) :
-            DataTableView<variable_table_record>(parent, id, position, size, validator) {
-        auto addVariableColumn = overloaded{
-            [&]<typename T, typename Formatter>(const variable_table_value<T, Formatter> &value) {
-                AddColumn<OptionalValueColumn<T, Formatter>>(value.header, value.column_width, std::ref(value));
-            },
-            [&]<typename T, typename TVal, typename Formatter>(const member_ptr_value<T, TVal, Formatter> &value) {
-                AddColumn<OptionalValueColumn<T, Formatter>>(value.header, value.column_width, value.value);
-            }
-        };
+        DataTableView<variable_table_record>(parent, id, position, size, validator)
+    {
         [&]<size_t ... Is>(std::index_sequence<Is...>) {
-            (addVariableColumn(std::get<Is>(dati_fattura)), ...);
+            ([&](const auto &value) {
+                using T = typename std::remove_reference_t<decltype(value)>::value_type;
+                using Formatter = typename std::remove_reference_t<decltype(value)>::formatter_type;
+                AddColumn<OptionalValueColumn<T, Formatter>> (value.header, value.column_width, std::ref(value));
+            }(std::get<Is>(dati_fattura)), ...);
         }(std::make_index_sequence<std::tuple_size_v<decltype(dati_fattura)>>{});
     }
 
     void AddReaderValues(const reader_output &data) {
-        variable_table_record current;
-        current.filename = data.filename.string();
-        current.status = string_join(data.warnings, "; ");
+        std::multimap<std::string, variable> current_map;
+
+        auto check_missing_add_item = [&]() {
+            variable_table_record record{std::move(current_map), data.filename.string(), data.warnings};
+            current_map.clear();
+
+            [&]<size_t ... Is>(std::index_sequence<Is...>) {
+                ([&](const auto &value) {
+                    if (value.required && !std::invoke(value, record)) {
+                        record.status.push_back(fmt::format("Mancante {}", value.header));
+                    }
+                }(std::get<Is>(dati_fattura)), ...);
+            }(std::make_index_sequence<std::tuple_size_v<decltype(dati_fattura)>>{});
+
+            AddItem(std::move(record));
+        };
+
         size_t table_idx = 0;
         for (const auto &[key, value] : data.values) {
             if (key.table_index == variable_key::global_index) continue;
             if (key.name.front() == '_') continue;
 
-            current.emplace(key.name, value);
             if (key.table_index != table_idx) {
-                AddItem(current);
-                current.clear();
+                check_missing_add_item();
                 table_idx = key.table_index;
             }
+            current_map.emplace(key.name, value);
         }
-        AddItem(current);
+        check_missing_add_item();
     }
 };
 
