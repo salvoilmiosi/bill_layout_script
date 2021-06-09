538ab8008cd486bd4c0bed3833945b6e3ebd54ba
Salvo Miosi <salvo.ilmiosi@gmail.com>
2021-06-09 15:51:13 +0200

    spostato editor e robe varie in repo separati

--- editor/src/clipboard.h
+++
@@ -1,4 +0,0 @@
-#include "layout.h"
-
-bool SetClipboard(const layout_box &box);
-bool GetClipboard(layout_box &box);
--- editor/src/pdf_to_image.h
+++
@@ -1,6 +0,0 @@
-
-#include "pdf_document.h"
-
-#include <wx/image.h>
-
-wxImage pdf_to_image(const pdf_document &doc, int page, int rotation = 0);
